08f222049b3c498c963aa9a19bfcf134b640f79f
DESKTOP-9N569U1\SAMSUNG <seojoonrp@snu.ac.kr>
2025-09-22 21:46:45 +0900

    docs: Add comments to APICaller.cs

--- Assets/01_Scripts/APICaller.cs
+++ Assets/01_Scripts/APICaller.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
 
+// API 서버에서 받아오는 메세지를 담을 클래스. JSON key 이름과 일치해야 한다.
+// JSON 등의 데이터로 변환할 수 있는 객체임을 알려주는 어트리뷰트
 [System.Serializable]
 public class ServerMessage
 {
@@ -24,6 +26,8 @@ public class APICaller : MonoBehaviour
     {
         using UnityWebRequest webRequest = UnityWebRequest.Get(url);
 
+        // 서버에 웹 요청을 보내고, 유니티에게 제어권을 넘겨준다.
+        // 응답이 오면 멈췄던 부분부터 코드를 다시 실행함
         yield return webRequest.SendWebRequest();
 
         if (webRequest.result == UnityWebRequest.Result.ConnectionError)
